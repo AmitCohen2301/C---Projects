b8c0e2ed8a0806bc80076a6db803cbb7d6959453
Amit Cohen <100520321+AmitCohen2301@users.noreply.github.com>
2022-06-19 01:41:39 +0300

    Delete StringFromFile directory

--- StringFromFile/READ_ME.txt
+++
@@ -1,2 +0,0 @@
-Information:
-This project manage a reading string char by char from binary file (there is a name of file in StringFromFile.h) and append it char by char. 
--- StringFromFile/StringFromFile.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define FILE_NAME "tavs.bin"
-
-void readAndPrintStringFromFile();					//Read and print string from file
-char* createStringFromFile(const char* fileName);	//Read string from binary file and add string char by char
-char* appendCharToString(char* str, char tav);		//Add char to string
